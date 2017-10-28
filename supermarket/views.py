4de18bca699bf2dd11c0755072cbfb39e835b4a2
kaiserthiago <thiagokaiser@oulook.com>
2017-10-28 11:33:48 -0400

    categoria promoção ok 2

--- supermarket/views.py
+++ supermarket/views.py
@@ -116,7 +116,7 @@ def produtos(request):
 
 
 def promocoes(request):
-    todas_categorias = Promocao.objects.filter(data_fim__gte=date.today()).order_by('produto__categoria__descricao').distinct()
+    todas_categorias = Promocao.objects.filter(data_fim__gte=date.today()).order_by('produto__categoria__descricao').values('produto__categoria', 'produto__categoria__descricao').distinct()
     todos_produtos = Promocao.objects.filter(data_fim__gte=date.today()).order_by(
         'data_fim', 'produto__descricao', 'valor', 'cliente__nome_fantasia')
     clientes = Cliente.objects.all()
