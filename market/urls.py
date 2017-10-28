b3fc4cea5e16957f7a239296a9cf2761c95ae68d
kaiserthiago <thiagokaiser@oulook.com>
2017-10-28 15:26:59 -0400

    maps

--- market/urls.py
+++ market/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include, static
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 
 
 from market import settings
-from supermarket import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls, name='market.admin'),
