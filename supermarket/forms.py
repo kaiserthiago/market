81c1a1c5ad551612e999713b2a55cec9a0dc8159
kaiserthiago <thiagokaiser@outlook.com>
2017-11-14 10:56:04 -0400

    admin

--- supermarket/forms.py
+++ supermarket/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-from bootstrap_datepicker.widgets import DatePicker
 
 from supermarket.models import Promocao, Produto, UserProfile
 
